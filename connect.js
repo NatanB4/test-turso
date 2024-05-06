51486a980c2402154e0760d692ffe566ff1121b7
natanb4 <natanbarbosa027@gmail.com>
2024-05-06 16:01:14 -0300

    start

---
+++ connect.js
@@ -0,0 +1,9 @@
+import { createClient } from "@libsql/client";
+
+const client = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
+
+await client.execute("SELECT * FROM users");
